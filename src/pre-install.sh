fb2c2c156443f270089e2b67f604bcec19f631be
Charles Beaumont <charles@lillehummer.nl>
2016-08-13 18:58:44 +0200

    Added installation scripts.

--- src/pre-install.sh
+++ src/pre-install.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
-wp core download
+
+# Install WordPress if not present
+if ! $(wp core is-installed); then
+  egrep -lR --exclude='pre-install.sh' 'lillehummernl' . | xargs sed -i '' -e "s/lillehummernl/${PWD##*/}/g"
+  sh ./src/wp-install.sh
+fi
+
+if [ ! -d wp-content/themes/${PWD##*/} ]; then
+  printf '\e[0;96m Cloning theme...\e[0m\n'
+  git clone --depth=1 --branch=master git@github.com:lillehummer/exoskeleton.git wp-content/themes/${PWD##*/}
+  rm -rf wp-content/themes/${PWD##*/}/.git
+  printf '\e[0;96m Replacing strings in theme...\e[0m\n'
+  egrep -lR --exclude='pre-install.sh' 'lillehummernl' . | xargs sed -i '' -e "s/lillehummernl/${PWD##*/}/g"
+  printf '\e[0;96m Activating theme...\e[0m\n'
+  wp theme activate ${PWD##*/}
+fi
+
+cd wp-content/themes/${PWD##*/}
+printf '\e[0;96m Updating NPM...\e[0m\n'
+# npm install
+printf '\e[0;96m Updating Bower...\e[0m\n'
+# bower install
---
+++ src/wp-install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+printf '\e[0;96m Downloading WordPress...\e[0m\n'
+wp core download
+
+printf '\e[0;96m Creating database...\e[0m\n'
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${PWD##*/};"
+
+printf '\e[0;96m Installing WordPress...\e[0m\n'
+wp core config --dbname=${PWD##*/} --dbuser=root --dbpass=
+wp core install --url=${PWD##*/}.dev --title=$sitename --admin_user=local --admin_password=local --admin_email=local@local.local
